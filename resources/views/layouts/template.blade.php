5d01e38bc2d26388d65f26d4180706af6d04f85b
Naditya206 <aditandino2019211@gmail.com>
2025-03-27 19:27:12 +0700

    Jobsheet 7 Retry Commit

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>{{ config('app.name', 'PWL Laravel Starter Code') }}</title>
+    <title>{{ config('app.name', 'PWL Laravel Starter Code.') }}</title>
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
