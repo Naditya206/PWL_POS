6fbad2fad0bc049197aaa747f9c8def100d78e87
Naditya206 <aditandino2019211@gmail.com>
2025-04-29 08:54:59 +0700

    TUGAS API PT 1

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,12 @@
 <?php
 
 use App\Http\Controllers\Api\RegisterController;
-use App\Http\Controllers\Api\LevelController;
-use App\Http\Controllers\Api\LogooutController;
 use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\LogoutController;
+use App\Http\Controllers\Api\LevelController;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\KategoriController;
+use App\Http\Controllers\Api\BarangController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,8 +21,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
-Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register'); 
+// route register
+Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register'); //[RegisterController::class, 'register']);
 
 // route login
 Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
@@ -36,3 +43,24 @@ Route::post('/levels', [LevelController::class, 'store']);
 Route::get('/levels/{level}', [LevelController::class, 'show']);
 Route::put('/levels/{level}', [LevelController::class, 'update']);
 Route::delete('/levels/{level}', [LevelController::class, 'destroy']);
+
+// Routes User
+Route::get('/users', [UserController::class, 'index']);
+Route::post('/users', [UserController::class, 'store']);
+Route::get('/users/{user}', [UserController::class, 'show']);
+Route::put('/users/{user}', [UserController::class, 'update']);
+Route::delete('/users/{user}', [UserController::class, 'destroy']);
+
+// Routes Kategori
+Route::get('/kategori', [KategoriController::class, 'index']);
+Route::post('/kategori', [KategoriController::class, 'store']);
+Route::get('/kategori/{kategori}', [KategoriController::class, 'show']);
+Route::put('/kategori/{kategori}', [KategoriController::class, 'update']);
+Route::delete('/kategori/{kategori}', [KategoriController::class, 'destroy']);
+
+// Routes Barang
+Route::get('/barang', [BarangController::class, 'index']);
+Route::post('/barang', [BarangController::class, 'store']);
+Route::get('/barang/{barang}', [BarangController::class, 'show']);
+Route::put('/barang/{barang}', [BarangController::class, 'update']);
+Route::delete('/barang/{barang}', [BarangController::class, 'destroy']);
